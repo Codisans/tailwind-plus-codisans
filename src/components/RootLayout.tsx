d311cdb0a0a314faa1294153867a6751d9f7e8f0
Sebastian Strand <sebastian@codisans.com>
2025-06-30 02:08:49 -0400

    fix: update language

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -90,7 +90,7 @@ function Header({
             EN
           </Link>
           <Button href="/contact" invert={invert}>
-            {t('Contact')}
+            {t('contact')}
           </Button>
           <button
             ref={toggleRef}
