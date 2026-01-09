16ccf14184dcbc9329f8f28090297c724a1c6c00
Sebastian Strand <sebastian@codisans.com>
2026-01-09 11:30:22 -0300

    fix: add cta to translations

--- src/app/[locale]/services/software-development/page.tsx
+++ src/app/[locale]/services/software-development/page.tsx
@@ -106,37 +106,6 @@ export default async function Laravel({
         items={services}
       />
 
-      {/* <div className="mt-24 sm:mt-32 lg:mt-40">
-        <Container>
-          <div className="lg:flex lg:items-center lg:justify-end lg:gap-x-8 xl:gap-x-20">
-            <div className="mt-12 lg:mt-0 lg:w-148 lg:flex-none">
-              <FadeIn className="max-w-md">
-                <h2 className="mt-2 font-display text-3xl font-medium tracking-tight text-theme-950 sm:text-4xl">
-                  {t('statamic.title')}
-                </h2>
-
-                <div className="mt-6 space-y-6 text-base text-theme-600">
-                  <p>{t('statamic.description')}</p>
-                </div>
-              </FadeIn>
-            </div>
-
-            <FadeIn className="w-120 flex-none lg:w-130">
-              <Image
-                alt="Statamic Logo"
-                src={statamicLogo}
-                sizes="(min-width: 1024px) 41rem, 31rem"
-                className="object-cover"
-              />
-            </FadeIn>
-          </div>
-        </Container>
-      </div> */}
-
-      {/* <Container className="mt-20 text-2xl">
-        <p>Add logos of platforms built on laravel for extra credibility</p>
-      </Container> */}
-
       <CaseStudyCardsBlock
         title={t('case-studies.title')}
         // summary={t('HomePage.case-studies.summary')}
@@ -159,7 +128,7 @@ export default async function Laravel({
         <p>{t('specialists.description')}</p>
       </SectionIntro>
 
-      <ContactBlock cta="Let's build" />
+      <ContactBlock cta={t('LaravelPage.contact.cta')} />
     </RootLayout>
   )
 }
